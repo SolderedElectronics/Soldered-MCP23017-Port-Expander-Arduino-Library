8e675a81fb620fce0184435660a0f86d8a8835e8
Docs Robot <robot@example.com>
2021-07-22 12:10:52 +0000

    Committing documentation changes

--- docs/annotated_dup.js
+++ docs/annotated_dup.js
@@ -4,7 +4,8 @@ var annotated_dup =
     [ "Hardware design", "index.html#autotoc_md2", null ],
     [ "Documentation", "index.html#autotoc_md3", null ],
     [ "About Soldered", "index.html#autotoc_md4", null ],
-    [ "Open-source license", "index.html#autotoc_md5", null ],
+    [ "Original source - UKLONI AKO NEMA", "index.html#autotoc_md5", null ],
+    [ "Open-source license", "index.html#autotoc_md6", null ],
     [ "EasyC", "class_easy_c.html", "class_easy_c" ],
     [ "MCP23017", "class_m_c_p23017.html", "class_m_c_p23017" ],
     [ "MCP_23017", "class_m_c_p__23017.html", "class_m_c_p__23017" ]
