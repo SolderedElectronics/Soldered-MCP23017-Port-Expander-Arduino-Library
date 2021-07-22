36f84edc4b54fc098b5d4e9b5095bb8320da65d5
Zvonimir Haramustek <nitko12@hotmail.com>
2021-07-22 09:28:27 +0200

    Initialize gh-pages

---
+++ docs/annotated_dup.js
@@ -0,0 +1,10 @@
+var annotated_dup =
+[
+    [ "Repository Contents", "index.html#autotoc_md1", null ],
+    [ "Hardware design", "index.html#autotoc_md2", null ],
+    [ "Documentation", "index.html#autotoc_md3", null ],
+    [ "About Soldered", "index.html#autotoc_md4", null ],
+    [ "Open-source license", "index.html#autotoc_md5", null ],
+    [ "EasyC", "class_easy_c.html", "class_easy_c" ],
+    [ "Sensor", "class_sensor.html", "class_sensor" ]
+];
