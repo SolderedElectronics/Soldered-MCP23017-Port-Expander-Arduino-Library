ff0415be25823d006988c05c321d0d12943f1e30
Docs Robot <robot@example.com>
2022-02-17 12:19:12 +0000

    Committing documentation changes

--- docs/hierarchy.js
+++ docs/hierarchy.js
@@ -1,7 +1,6 @@
 var hierarchy =
 [
-    [ "EasyC", "class_easy_c.html", null ],
-    [ "MCP23017_SOLDERED_LIBS::EasyC", null, [
+    [ "EasyC", "class_easy_c.html", [
       [ "MCP_23017", "class_m_c_p__23017.html", null ]
     ] ],
     [ "MCP23017", "class_m_c_p23017.html", [
